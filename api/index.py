3b5e89202eb243c7fc85808f16cf3ab3d386bc18
Ari <13222093@std.stei.itb.ac.id>
2025-11-28 10:13:44 +0700

    FastApi for vercel

---
+++ api/index.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+# Add the project root to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from services.api_gateway.src.main import app
